962af1104a3bd71ad51731c7bb86bc1ebcb223ea
aidatorajiro <kawarusosu@zoho.com>
2021-03-13 22:21:20 +0900

    add zst compressed out.json

--- small.py
+++ small.py
@@ -4,7 +4,7 @@ import random
 with open('out.json', 'r') as f:
     obj = json.load(f)
 
-N = 400
+N = 100
 
 with open('out.small.json', 'w') as f:
     json.dump(random.sample(obj, N), f, indent=1, ensure_ascii=False)
