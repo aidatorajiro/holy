96ef5a3fa245721abd45055968ec42823484f546
aidatorajiro <kawarusosu@zoho.com>
2021-03-15 19:08:50 +0900

    (background) change rendering method

--- background.js
+++ background.js
@@ -1,47 +1,50 @@
 class Background {
+    updateParams (uniforms) {
+        let common = 1/this.width/this.coeff;
+        uniforms.scale.value.x = Globals.width*common
+        uniforms.scale.value.y = Globals.height*common
+        uniforms.pos.value.x = Globals.camera.position.x*common
+        uniforms.pos.value.y = Globals.camera.position.y*common
+    }
     constructor () {
-        let coeff = 200; // param
-        let alpha = 2.2;
-        let beta_1 = 5723.9;
-        let beta_2 = 25925.9;
+        this.coeff = 200; // param
+        this.alpha = 2.2;
+        this.beta_1 = 5723.9;
+        this.beta_2 = 25925.9;
+
+        let coeff = this.coeff;
+        let alpha = this.alpha;
+        let beta_1 = this.beta_1;
+        let beta_2 = this.beta_2;
         
         var map = new THREE.TextureLoader().load( "assets/4.png", (tex) => {
-            let geometry = new THREE.PlaneGeometry(tex.image.width*coeff, tex.image.height*coeff, 1, 1);
+            this.width = tex.image.width;
+            this.height = tex.image.height;
+            let geometry = new THREE.PlaneGeometry(Globals.width, Globals.height, 1, 1);
+            let uniforms = {
+                texture: { value: tex },
+                far: { value: new THREE.Vector2(this.width*alpha, this.height*alpha) },
+                dotvec: { value: new THREE.Vector2(beta_1*coeff, beta_2*coeff) },
+                scale: {value: new THREE.Vector2()},
+                pos: {value: new THREE.Vector2()}
+            };
+            this.updateParams(uniforms);
             let material = new THREE.RawShaderMaterial({
-                uniforms: {
-                  texture: { value: tex },
-                  far: { value: new THREE.Vector2(tex.image.width*alpha, tex.image.height*alpha) },
-                  dotvec: { value: new THREE.Vector2(beta_1*coeff, beta_2*coeff) }
-                },
-                vertexShader: `
-precision mediump float;
-
-uniform mat4 modelViewMatrix;
-uniform mat4 projectionMatrix;
-attribute vec3 position;
-attribute vec2 uv;
-varying vec2 vUv;
-
-void main() {
-    vUv = uv;
-    vec3 pos = position;
-    gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);
-}
-`,
+                uniforms: uniforms,
+                vertexShader: Shaders.defaultVertexShader,
                 fragmentShader: `
 precision mediump float;
 
 uniform sampler2D texture;
 uniform vec2 far;
 uniform vec2 dotvec;
+uniform vec2 scale;
+uniform vec2 pos;
 varying vec2 vUv;
 void main() {
-    //float r = fract(sin(dot(vUv.xy ,vec2(17.0,7.0))) * 10000.0);
-    //float r = sin(dot(vUv.xy, vec2(171717.0,777777.0)));
-    //float r = sin(dot(vUv.xy, vec2(sin(vUv.x * 10000.0) * 10000.0,sin(vUv.y * 10000.0) * 10000.0)));
-    //float r = fract(sin(dot(vUv.xy, vec2(171717.0,777777.0))));
-    float r = sin(dot(vUv.xy, dotvec));
-    vec2 v = vec2(vUv.x + r/far.x, vUv.y + r/far.y);
+    vec2 vUvS = vUv*scale + (-scale + 1.0)/2.0 + pos;
+    float r = sin(dot(vUvS.xy, dotvec));
+    vec2 v = vec2(vUvS.x + r/far.x, vUvS.y + r/far.y);
     vec3 color = texture2D(texture, v).rgb*0.8;
     gl_FragColor = vec4(color, 1);
 }
@@ -53,6 +56,14 @@ void main() {
             Globals.scene.add(mesh);
     
             mesh.position.z = 0
+
+            this.mesh = mesh
         } );
+
+        Globals.event.addListener("animate", () => {
+            this.updateParams(this.mesh.material.uniforms)
+            this.mesh.position.x = Globals.camera.position.x
+            this.mesh.position.y = Globals.camera.position.y
+        })
     }
 }
