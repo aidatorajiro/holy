bd28e6447a918e139a4d175150b97f50a2844878
aidatorajiro <kawarusosu@zoho.com>
2021-03-25 07:57:22 +0900

    (movement) fix "jumping"

--- movement.js
+++ movement.js
@@ -7,18 +7,21 @@ class Movement {
         Globals.event.addListener("keyup", (ev) => {
             this.pressingkey = undefined
         });
+        let a = 40
+        let f = (x) => (Math.atan( ( 1 / ( a * 2 / Math.PI ) ) * x ) * a * 2 / Math.PI)
         Globals.event.addListener("animate", (ev) => {
+            let d = f(ev.delta)
             if (this.pressingkey === 'd') {
-                Globals.camera.position.x += 0.5*ev.delta
+                Globals.camera.position.x += 0.5*d
             }
             if (this.pressingkey === 'a') {
-                Globals.camera.position.x -= 0.5*ev.delta
+                Globals.camera.position.x -= 0.5*d
             }
             if (this.pressingkey === 'w') {
-                Globals.camera.position.y += 0.5*ev.delta
+                Globals.camera.position.y += 0.5*d
             }
             if (this.pressingkey === 's') {
-                Globals.camera.position.y -= 0.5*ev.delta
+                Globals.camera.position.y -= 0.5*d
             }
         });
     }
