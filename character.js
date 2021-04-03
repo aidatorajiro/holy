034ae486f122e6fa5a6f80d77e696552b2bae319
aidatorajiro <kawarusosu@zoho.com>
2021-04-03 15:23:51 +0900

    (character) add animate

--- character.js
+++ character.js
@@ -1,4 +1,8 @@
 class Character {
     constructor () {
+        this.special_emojis = []
+        Globals.event.addListener("animate", () => (this.animate.call(this)))
+    }
+    animate () {
     }
 }
