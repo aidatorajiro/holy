af9c8a731c107b269e68640882c2c0b4eee7dd00
aidatorajiro <kawarusosu@zoho.com>
2021-04-03 15:23:27 +0900

    remove unused function

--- utils.js
+++ utils.js
@@ -1,18 +1,4 @@
 let Utils = {
-    textureFromImage: function (src) {
-        return new Promise(function (res, rej) {
-            let canvas = document.createElement('canvas');
-            let ctx = canvas.getContext('2d');
-            let im = document.createElement("img");
-            im.src = src;
-            im.addEventListener('load', function (e) {
-                canvas.width = this.width;
-                canvas.height = this.height;
-                ctx.drawImage(im, 0, 0);
-                res(canvas);
-            })
-        });
-    },
     rnd: function (str) {
         let seed = this.xmur3(str);
         return this.mulberry32(seed())
