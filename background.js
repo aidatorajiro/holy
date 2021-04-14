0ab672f0392effb22258fbd387763712b1d69ab6
aidatorajiro <kawarusosu@zoho.com>
2021-04-14 09:14:35 +0900

    (background) change params

--- background.js
+++ background.js
@@ -13,9 +13,9 @@ class Background {
             [
                 "assets/4s.png",
                 200,
-                2.2,
-                5723.9,
-                25925.9
+                7.5,
+                6000,
+                56700
             ],
             [
                 "assets/4.png",
@@ -75,22 +75,22 @@ class Background {
                 uniforms: uniforms,
                 vertexShader: Shaders.defaultVertexShader,
                 fragmentShader: `
-    precision mediump float;
+precision mediump float;
 
-    uniform sampler2D texture;
-    uniform vec2 far;
-    uniform vec2 dotvec;
-    uniform vec2 scale;
-    uniform vec2 pos;
-    varying vec2 vUv;
-    void main() {
-    vec2 vUvS = vUv*scale + (-scale + 1.0)/2.0 + pos;
-    float r = sin(dot(vUv.xy, dotvec));
-    vec2 v = fract(vec2(vUvS.x + r/far.x, vUvS.y + r/far.y));
-    vec3 color = texture2D(texture, v).rgb*0.7 + 0.15;
-    gl_FragColor = vec4(color, 1);
-    }
-    `,
+uniform sampler2D texture;
+uniform vec2 far;
+uniform vec2 dotvec;
+uniform vec2 scale;
+uniform vec2 pos;
+varying vec2 vUv;
+void main() {
+vec2 vUvS = vUv*scale + (-scale + 1.0)/2.0 + pos;
+float r = sin(dot(vUv.xy, dotvec));
+vec2 v = fract(vec2(vUvS.x + r/far.x, vUvS.y + r/far.y));
+vec3 color = texture2D(texture, v).rgb*0.7 + 0.15;
+gl_FragColor = vec4(color, 1);
+}
+`,
                 transparent: true,
             });
 
