d0b92ac333cba5e464cb7faca5a05e6618ba7ffb
aidatorajiro <kawarusosu@zoho.com>
2021-04-15 08:56:12 +0900

    (movement) multiple keys

--- movement.js
+++ movement.js
@@ -1,30 +1,32 @@
 class Movement {
     constructor () {
-        this.pressingkey = undefined
+        this.pressingkeys = new Set()
         Globals.event.addListener("keydown", (ev) => {
-            this.pressingkey = ev.key
+            this.pressingkeys.add(ev.key)
         });
         Globals.event.addListener("keyup", (ev) => {
-            this.pressingkey = undefined
+            this.pressingkeys.delete(ev.key)
         });
         let a = 40
         let f = (x) => (Math.atan( ( 1 / ( a * 2 / Math.PI ) ) * x ) * a * 2 / Math.PI)
         let rnd = Utils.rnd("movement.js precious seed " + Math.random())
         Globals.event.addListener("animate", (ev) => {
-            let d = f(ev.delta)
-            let r = rnd()
-            let c = 0.5 + (r - 0.5)*0.4
-            if (this.pressingkey === 'd') {
-                Globals.camera.position.x += d*c
-            }
-            if (this.pressingkey === 'a') {
-                Globals.camera.position.x -= d*c
-            }
-            if (this.pressingkey === 'w') {
-                Globals.camera.position.y += d*c
-            }
-            if (this.pressingkey === 's') {
-                Globals.camera.position.y -= d*c
+            for (let k of this.pressingkeys) {
+                let d = f(ev.delta)
+                let r = rnd()
+                let c = 0.5 + (r - 0.5)*0.4
+                if (k === 'd') {
+                    Globals.camera.position.x += d*c
+                }
+                if (k === 'a') {
+                    Globals.camera.position.x -= d*c
+                }
+                if (k === 'w') {
+                    Globals.camera.position.y += d*c
+                }
+                if (k === 's') {
+                    Globals.camera.position.y -= d*c
+                }
             }
         });
     }
